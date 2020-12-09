a147364d394c0c7160496473a907b54996d5e1c9
Maxime <maxime.gavens@gmail.com>
2020-12-09 10:39:43 +0100

    feat(recommendation): update description, and result format

--- recommandation/rsclusterbased.py
+++ recommandation/rsclusterbased.py
@@ -85,6 +85,9 @@ class RSClusterBased:
         else:
             self.__load_predicted_data()
 
+    def get_raw_df(self):
+        return self.__raw_df
+
     def __load_predicted_data(self):
         if not user_proc_cluster_file.is_file():
             raise Exception("Predicted data doesn't exist. Re-compute the prediction.")
@@ -103,19 +106,74 @@ class RSClusterBased:
             self.__data.to_pickle(user_proc_cluster_file)
             print("File successfully saved to <PATH_TO_PATH>/processed-data/user_proc_cluster.pkl")
 
-    def __get_most_buy_from_cluster_id(self, cluster_id):
-        # filter the processed data by the number of the cluster given, then retrieve index client corresponding
-        cluster_user_ids = list(self.__data[self.__data["prediction"] == cluster_id]["CLI_ID"])
+    def __get_customers_id_from_cluster_id(self, cluster_id):
+        """
+        filter the processed data by the number of the cluster given, then retrieve index client corresponding
+        :param cluster_id:
+        :return: list of id
+        """
+        return list(self.__data[self.__data["prediction"] == cluster_id]["CLI_ID"])
 
-        # from the client index of the cluster, retrieve all the transactions made by those clients
-        filtered_by_user_ids = self.__raw_df[self.__raw_df["CLI_ID"].isin(cluster_user_ids)]
+    def __proportion_to_string(self, prop):
+        s = []
+        for key, val in prop.items():
+            if val != "0.0%":
+                s.append(f"{key}: {val}")
+        return ", ".join(s)
+
+    def __get_famille_proportion(self, customer_ids):
+        famille = ['HYGIENE', 'SOINS DU VISAGE', 'PARFUMAGE', 'SOINS DU CORPS', 'MAQUILLAGE', 'CAPILLAIRES',
+                   'SOLAIRES', 'MULTI FAMILLES', 'SANTE NATURELLE']
+        result = {}
+
+        filtered = self.__data[self.__data["CLI_ID"].isin(customer_ids)]
+        tot = 0.0
+        for col in famille:
+            s = filtered[col].sum()
+            tot = tot + s
+            result[col] = s
+        for col in famille:
+            result[col] = str(round(result[col] * 100.0 / tot, 2)) + '%'
+        return result
+
+    def __get_purchases_from_customer_ids(self, ids):
+        # from the client IDs, retrieve all the transactions made by those clients
+        filtered_by_user_ids = self.__raw_df[self.__raw_df["CLI_ID"].isin(ids)]
 
         # get only the name of the purchased product, then count their value
         return filtered_by_user_ids["LIBELLE"].value_counts()
 
-    def __get_prod_buy_from_user_id(self, user_id):
+    def __get_purchases_unique_from_customer_id(self, user_id):
         filtered = self.__raw_df[self.__raw_df["CLI_ID"] == user_id]
-        return list(filtered["LIBELLE"].unique())
+        return set(filtered["LIBELLE"].unique())
+
+    def __complete_dict_customer_purchase(self, related_customers, customer_id):
+        related_customers[str(customer_id)] = {
+            "purchases": {}
+        }
+        # get items buying by the customers
+        customer_transaction: pd.DataFrame = self.__raw_df[self.__raw_df["CLI_ID"] == customer_id]
+        for item, count in customer_transaction["LIBELLE"].value_counts().items():
+            related_customers[str(customer_id)]["purchases"][item] = count
+
+    def __count_df_to_json(self, count):
+        return [{"LIBELLE": x, "occurrence": y} for x, y in count.items()]
+
+    def __compute_explanation(self, data):
+        first = data["recommendations"][0]["LIBELLE"]
+        user_id = data["current_customer"]["ID"]
+        current_proportion = self.__proportion_to_string(data["current_customer"]["proportions"])
+        cluster_proportion = self.__proportion_to_string(data["cluster_customer"]["proportions"])
+        cluster_size = len(data["cluster_customer"]["IDs"])
+        number_of_user = 0
+        for other_id in data["cluster_customer"]["IDs"]:
+            if first in self.__get_purchases_unique_from_customer_id(other_id):
+                number_of_user = number_of_user + 1
+        prop = round(number_of_user * 100.0 / cluster_size, 2)
+
+        return f"{first} is the best recommendation for the customer {user_id}, because {prop}% of his cluster buy " \
+               f"this product too. Furthermore the user and his cluster have the same type of consumption: *User " \
+               f"consumption type: {current_proportion} ; *Cluster consumption type: {cluster_proportion} ."
 
     def get_recommendation(self, user_id, n=10):
         """
@@ -124,28 +182,42 @@ class RSClusterBased:
         :param n: size of the product list recommended
         :return: a list of most buy element, the first is the most recommended
         """
-        # Get the cluster number of the user
-        cluster_id = self.__data[self.__data["CLI_ID"] == user_id]["prediction"].iloc[0]
+        # Get the cluster label of the user
+        cluster_label = self.__data[self.__data["CLI_ID"] == user_id]["prediction"].iloc[0]
+
+        cluster_ids = self.__get_customers_id_from_cluster_id(cluster_label)
 
         # Get a list of the most buy product (ordered and indexed by their number)
-        most_buy_cluster = self.__get_most_buy_from_cluster_id(cluster_id)
-        print(f"Size of most buy products from cluster: {len(most_buy_cluster)}")
+        other_purchases = self.__get_purchases_from_customer_ids(cluster_ids)
 
         # Get a list of product purchased by a customer
-        from_user = self.__get_prod_buy_from_user_id(user_id)
+        user_purchases = self.__get_purchases_from_customer_ids([user_id])
+        user_purchases_unique = self.__get_purchases_unique_from_customer_id(user_id)
 
-        i = 0
-        target = []
-        for index, values in most_buy_cluster.items():
-            if index not in from_user:
-                target.append(index)
-            if i >= n:
-                break
-            i = i + 1
-        return target
+        # remove products already purchased by the current customer
+        other_purchases_without_current = other_purchases[~other_purchases.index.isin(user_purchases_unique)]
+
+        data = {
+            "current_customer": {
+                "ID": user_id,
+                "proportions": self.__get_famille_proportion([user_id]),
+                "purchases": self.__count_df_to_json(user_purchases)
+            },
+            "cluster_customer": {
+                "IDs": cluster_ids,
+                "proportions": self.__get_famille_proportion(cluster_ids),
+                "purchases": self.__count_df_to_json(other_purchases)
+            },
+            "recommendations": self.__count_df_to_json(other_purchases_without_current),
+        }
+
+        explanation = self.__compute_explanation(data)
+        data["explanation"] = explanation
+
+        return data
 
 
 if __name__ == "__main__":
     rs: RSClusterBased = RSClusterBased()
     prediction = rs.get_recommendation(1490281)
-    print(prediction)
+    print(json.dumps(prediction, indent=2))
